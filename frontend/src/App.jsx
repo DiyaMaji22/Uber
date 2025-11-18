71ca43fa4e9e0a21def33256022866b561e43a09
Diya Maji <diyamaji22@gmail.com>
2025-11-18 23:22:33 +0530

    setting up the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+ import { Routes,Route } from 'react-router-dom'
+ import Home from './pages/Home.jsx'
+ import Userlogin from './pages/userlogin.jsx'
+  import UserSignup from './pages/UserSignup.jsx'
+  import Captainlogin from './pages/Captainlogin.jsx'
+  import CaptainSignup from './pages/CaptainSignup.jsx'
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/userlogin' element={<Userlogin />}></Route>
+        <Route path='/usersignup' element={<UserSignup />}></Route>
+        <Route path='/captainlogin' element={<Captainlogin />}></Route>
+        <Route path='/captainsignup' element={<CaptainSignup />}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App 
---
+++ frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Userlogin = () => {
+  return (
+    <div>userlogin</div>
+  )
+}
+
+export default Userlogin
