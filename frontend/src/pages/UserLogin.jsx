6324e89842f225fc6dcfaf682d9a74ed02d65657
Diya Maji <diyamaji22@gmail.com>
2025-11-19 23:07:56 +0530

    home page done

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Userlogin = () => {
   return (
-    <div>userlogin</div>
+    <div className='p-7'>
+     <form>
+        <h3 className='text-xl mb-2'>What's your email</h3>
+        <input required className='bg-gray-200 rounded px-4 py-2 border-gray-200 w-full text-lg placeholder:text-sm mb-7' type="email" placeholder="email@example.com"/>
+        <h3 className='text-xl mb-2'>Enter Password</h3>
+        <input required className='bg-gray-200 rounded px-4 py-2 border-gray-200 w-full text-lg placeholder:text-sm mb-7' type="password" placeholder='password'/>
+        <button className='bg-black rounded px-4 py-2 border-gray-200 w-full text-lg placeholder:text-sm mb-7 text-white' >Login</button>
+     </form>
+      
+    </div>
   )
 }
 
